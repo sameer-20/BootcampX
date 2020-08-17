1cf4861413f283d0e9fbd50a069f5e9829578ae1
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-08-17 22:11:11 +0000

    Adding Group By queries

---
+++ 3_queries_group-by/3_find_large_cohorts.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name AS cohort_name, COUNT(students.name) AS student_count
+FROM cohorts
+JOIN students
+ON cohorts.id = students.cohort_id
+GROUP BY cohort_name
+HAVING COUNT(students.name) >= 18
+ORDER BY student_count;
---
+++ 3_queries_group-by/6_low_average_completion_time.sql
@@ -0,0 +1,11 @@
+SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration,
+AVG(assignments.duration) as average_estimated_duration
+FROM assignment_submissions
+JOIN students
+ON student_id = students.id 
+JOIN assignments
+ON assignment_id = assignments.id
+WHERE students.end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
